66e2a29badc1b217556e375146e589a47569d79d
destawiji <48589979+destawiji@users.noreply.github.com>
2019-03-15 21:04:05 +0700

    index.php
    script untuk menampilkan kalimat "Hello World" menggunakan bahasa PHP

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>Information Retrieval 2019</title>
+		<link rel="icon" href="favicon.ico">
+		<style>
+			h1 {
+				color: #fff;
+				text-align: center;
+				margin-top: 10%;
+			}
+			body {
+				background-color: black;
+			}
+		</style>
+	</head>
+	<body>
+		<h1> <?= 'Hello World'; ?> </h1>
+	</body>
+</html>
